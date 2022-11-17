3fd0a0e524d49bfd5bf57bcdc542ba73404e7d96
beerose <alexsandra.sikora@gmail.com>
2022-11-17 13:46:15 +0800

    Cleanup for deploy

--- astro.config.ts
+++ astro.config.ts
@@ -4,10 +4,10 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
-
 import { rehypePlugins, remarkPlugins } from "./src/build-time";
 
 const __filename = fileURLToPath(import.meta.url);
+
 const __dirname = dirname(__filename);
 
 // https://astro.build/config
@@ -15,25 +15,21 @@ export default defineConfig({
   markdown: {
     extendDefaultPlugins: true,
     // We'll highlight using Shiki Twoslash remark plugin
-    syntaxHighlight: false,
+    syntaxHighlight: false
   },
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    mdx({
-      // MDX integration inherits all remark plugins from markdown.remarkPlugins
-      extendPlugins: "markdown",
-      remarkPlugins: remarkPlugins(__dirname),
-      rehypePlugins: rehypePlugins,
-    }),
-    solidJs(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx({
+    // MDX integration inherits all remark plugins from markdown.remarkPlugins
+    extendPlugins: "markdown",
+    remarkPlugins: remarkPlugins(__dirname),
+    rehypePlugins: rehypePlugins
+  }), solidJs()],
   vite: {
     ssr: {
-      noExternal: ["@fontsource/inter", "@fontsource/brygada-1918"],
-    },
+      noExternal: ["@fontsource/inter", "@fontsource/brygada-1918"]
+    }
   },
 });

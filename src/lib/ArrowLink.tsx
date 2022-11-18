43b2345026cf30113bfd6f4575e8674355d1cf1c
beerose <alexsandra.sikora@gmail.com>
2022-11-18 13:59:35 +0800

    Small layout improvements

--- src/lib/ArrowLink.tsx
+++ src/lib/ArrowLink.tsx
@@ -10,7 +10,7 @@ export function ArrowLink(props: ArrowLinkProps) {
   return (
     <a
       class={
-        "hover:translate-x-1 hover:underline underline-offset-4 decoration-gray-500 decoration-1 duration-100 whitespace-nowrap rounded-sm text-xs text-gray-600 dark:text-gray-400 my-4 no-underline " + props.class
+        "hover:translate-x-1 hover:underline underline-offset-4 decoration-gray-500 decoration-1 decoration-wavy duration-100 whitespace-nowrap rounded-sm text-xs text-gray-600 dark:text-gray-400 my-4 no-underline " + props.class
       }
       href={props.href}
     >

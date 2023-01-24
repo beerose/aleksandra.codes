db499bdec40c983ffc4fba8444caa1bd04070509
beerose <alexsandra.sikora@gmail.com>
2023-01-24 15:28:30 +0100

    Update ci scripts

--- .github/scripts/deploy.mjs
+++ .github/scripts/deploy.mjs
@@ -10,7 +10,7 @@ const { token, prod } = parseArgs(process.argv.slice(2));
 // 1. Pull vercel environment information
 // 2. Build project artifacts
 // 3. Deploy project artifacts to vercel
-// 4. Alias deployment to ${branch}--zaduma.vercel.app and write to GITHUB_ENV
+// 4. Alias deployment to ${branch}--aleksandra-codes.vercel.app and write to GITHUB_ENV
 execSync(
   `\
   vercel pull \
@@ -49,5 +49,5 @@ function createDeploymentAlias() {
     .replace(/-+/g, "-")
     .toLowerCase();
 
-  return `${refSlug}--zaduma.vercel.app`;
+  return `${refSlug}--aleksandra-codes.vercel.app`;
 }

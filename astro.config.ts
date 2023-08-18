f731e1963f0040051cb4c546f7d2d2f348b499de
beerose <alexsandra.sikora@gmail.com>
2023-08-18 12:58:08 +0200

    hack v2

--- astro.config.ts
+++ astro.config.ts
@@ -59,7 +59,7 @@ function makePublicURL() {
 
   if (!DEPLOYMENT_ALIAS) {
     // If there's no DEPLOYMENT_ALIAS nor VERCEL_URL, we assume we're building locally.
-    return "http://aleksandra.codes/";
+    return "https://aleksandra.codes/";
   }
 
   // Otherwise, we build on GitHub Actions (and get access to Git History).
